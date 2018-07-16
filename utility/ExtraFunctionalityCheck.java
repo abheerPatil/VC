3fafa5a7bf0826e83d70eefd8a1246506c2022cb
Rishabh Patil <rishabhpatil6@hotmail.com>
2018-07-16 19:21:59 +0530

    ExtraFunctionality - Utility
    3 Methods

--- utility/ExtraFunctionalityCheck.java
+++ utility/ExtraFunctionalityCheck.java
@@ -1,15 +1,28 @@
 package utility;
 import java.io.File;
-import java.util.ArrayList;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
 
 
 
-public class ExtraFunctionalityCheck {
+public class ExtraFunctionalityCheck {	
+/*********************************************************************METHODS**********************************************************************************************/	
+	/*METHODS COUNT: 3
+	 * 1. getNumberFromString(String)
+	 * 2. getScreenshots(MobileElement element)
+	 * 3. wait(MobileElement element, IOSDriver driver) 
+	 */
+/***************************************************************/	
+	
+	/*Method to convert a String representation of numbers into Integer.
+	 * Parameters : String 
+	 * Returns : Integer representation of String else 0 if there is an exception.
+	 */
 	
 	public static int getNumberFromString(String s){
 		try{
@@ -25,6 +38,11 @@ public class ExtraFunctionalityCheck {
 		}
 	}
 	
+	/*Method to get screenshot.
+	 * Parameters : MobileElement 
+	 * Returns : screenshot of the screen in the file destination assigned below.
+	 */
+	
 	public static void getScreenshots(MobileElement element){
 		  try{
 			File screenshot = element.getScreenshotAs(OutputType.FILE);
@@ -34,6 +52,23 @@ public class ExtraFunctionalityCheck {
 		  catch(Exception e){
 			  System.err.println("Exception in class - ExtraFunctionalityCheck, in method - getScreenshots :"+e);
 		  }
+	}
+	
+	/*Method for explicit wait.
+	 * Parameters : MobileElement, IOSDriver 
+	 * Returns : null.
+	 */
+	
+	public static void wait(MobileElement element, IOSDriver<MobileElement>driver){
+		try{
+			WebDriverWait wait = new WebDriverWait(driver, 20);
+			wait.until(ExpectedConditions.visibilityOf(element));
+		}
+		catch(Exception e){
+			System.err.println("Exception in class - ExtraFunctionalityCheck, in method - wait :"+e);
+		}
+		
+		
 	}
 
 }
