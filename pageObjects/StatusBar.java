c2b843ccd798e665535e55f9a216eee1e51a7ca5
Rishabh Patil <rishabhpatil6@hotmail.com>
2018-07-27 17:23:14 +0530

    StatusBar - POM
    10 elements and 2 actions

--- pageObjects/StatusBar.java
+++ pageObjects/StatusBar.java
@@ -1,5 +1,7 @@
 package pageObjects;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.MobileElement;
@@ -12,13 +14,17 @@ public class StatusBar {
 	
 	IOSDriver<MobileElement> driver;
 	
-	private StatusBar(IOSDriver<MobileElement> driver){
+/****************************************************************************CONSTRUCTOR************************************************************************************/	
+	
+	public StatusBar(IOSDriver<MobileElement> driver){
 		System.out.println("Started");
 		this.driver = driver;
-		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-		//PageFactory.initElements(new AjaxElementLocatorFactory(driver,20), this);
+		PageFactory.initElements(new AppiumFieldDecorator(driver, 30, TimeUnit.SECONDS), this);
 	}
-	
+
+/****************************************************************************ELEMENTS***************************************************************************************/	
+	//ELEMENTS COUNT : 10
+/***************************/			
 	@iOSFindBy(xpath="//XCUIElementTypeOther[contains(@name,'iPad')]")
 	private static IOSElement iPad; 
 	
@@ -31,12 +37,40 @@ public class StatusBar {
 	@iOSFindBy(xpath="//XCUIElementTypeOther[contains(@name,'battery power, Charging')]")
 	private static IOSElement charging; 
 	
+
+/****************************************************************************ACTIONS***************************************************************************************/	
+	/*ACTIONS COUNT: 2
+	 * 1. isDisplayed()
+	 * 2. isOffline()
+	 */
+/*******************************************************/
+	
+	/*Method to check whether status bar is visible or not.
+	 * Parameters : null 
+	 * Returns : true if visible , false otherwise.
+	 */
 	public static boolean isDisplayed(){
-		
+		try{
 		return ((iPad.isDisplayed())&&(wifiBar.isDisplayed())&&(time.isDisplayed())&&(charging.isDisplayed()));
+		}
+		catch(Exception e){
+			System.err.println("Exception in class - StatusBar, in method - isDisplayed : "+e);
+			return false;
+		}
 	}
 	
+	/*Method to check wi-fi is available or not.
+	 * Parameters : null 
+	 * Returns : true if available , false otherwise.
+	 */
 	public static boolean isOffline(){
-		return (wifiBar.isDisplayed());
+		try{
+			wifiBar.isDisplayed();
+			return true;
+		}
+		catch(Exception e){
+			System.err.println("Exception in class - StatusBar, in method - isOffline : "+e);
+			return false;
+		}
 	}
 }
